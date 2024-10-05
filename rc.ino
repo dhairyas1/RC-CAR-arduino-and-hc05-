2a41b4e3af3083d8584fb361bd800cd65e1ada9e
dhairya <119152782+dhairyas1@users.noreply.github.com>
2024-10-05 16:29:31 +0530

    Update rc.ino

--- rc.ino
+++ rc.ino
@@ -1,4 +1,6 @@
 #include <SoftwareSerial.h>
+
+// Define SoftwareSerial pins for HC-05
 SoftwareSerial BTSerial(10, 11);  // RX | TX (Connect HC-05 TX to Pin 10, HC-05 RX to Pin 11)
 
 int leftMotorPin1 = 8;  // IN1 for left motor
@@ -19,7 +21,9 @@ int rightv =0;
 void setup() {
   Serial.begin(9600);   // Serial Monitor
   BTSerial.begin(9600); // HC-05
+
   Serial.println("Bluetooth Ready");
+
   pinMode(leftMotorPin1, OUTPUT);
   pinMode(leftMotorPin2, OUTPUT);
   pinMode(rightMotorPin1, OUTPUT);
@@ -30,83 +34,99 @@ void setup() {
 
 void loop() {
   // Check if data is available from the HC-05 Bluetooth module
+
+  if (!BTSerial.available()) {
+    analogWrite(leftMotorEnable, 0);
+    analogWrite(rightMotorEnable, 0);
+  }
+
   while (BTSerial.available()) {
     char c = BTSerial.read(); // Read a single character from Bluetooth
-    Serial.print(c);       // Print received character (debugging)
-    if(c=='\n')
-      {
-        readingX = true;
-        Serial.println("new");
+    //Serial.print(); // Print label
+    //Serial.print(c);       // Print received character
+    if(readingX){
+      if(c==':'){
+        readingX=false;
+      }else{
+        inputX += String(c);
+      }
+    }else{
+      if(c=='\n'){
         break;
+      }else{
+        inputY += String(c);
       }
-    else if(readingX)
-      {
-        inputX += String(c); //concatinating x input from joystick app.
-      }
-    else if (!readingX){
-        inputY += String(c); //concatenating y input from joystick app.
-      }
-    else if(c==':') //x axis and y axis coordintes are seperated by colons
-      {
-        readingX = false;
-      }
+    }
   }
-  //type casting string to integral values
-   int x = inputX.toInt(); 
+
+
+  //Serial.println(inputX+inputY);
+   int x = inputX.toInt();
    int y = inputY.toInt();
-  //in the case a blank string is converted to int
-  if(x==0)
-  {
-     x = 140; 
+
+   //int ny = map(y,218, 62 ,-255, 255);
+
+   //int nx = map(x, 62 , 218 ,-255, 255);
+
+  if (y == 0){
+    y= 140;
   }
-  if(y==0)
-  {
-     y = 140; 
+  if (x == 0){
+    x= 140;
   }
-  //mapping values received to the left and right motors
   
-   leftv= map(y + x, 436, 124 , -255, 255); 
-   rightv= map(y - x,-156 ,156 ,-255, 255);
-  //passing the values mapped to motors and ensuring negative values result in the motor running backwards
-   if (leftv > 5 ){
-     analogWrite(leftMotorEnable, leftv);
-     digitalWrite(leftMotorPin1, HIGH);
-     digitalWrite(leftMotorPin2, LOW);
-   }
-   else if (leftv < 5 ){
-     analogWrite(leftMotorEnable, -leftv);
-     digitalWrite(leftMotorPin1, LOW);
-     digitalWrite(leftMotorPin2, HIGH);
-   }
-   else
-   {
-     analogWrite(leftMotorEnable, 0);
-     digitalWrite(leftMotorPin1, LOW);
-     digitalWrite(leftMotorPin2, LOW);
+  //  leftv= map(y + x, 436, 124 , -255, 255);
+  //  rightv= map(y - x,-156 ,156 ,-255, 255);
+  // Serial.println(inputX);
+  // Serial.println(inputY);
+    y -= 140;
+    x -= 140;
 
-   }
+    y = map(y, 80 , -80, -255, 255);
+    x = map(x, -80 , 80, -255, 255);
+   //int ny = map(y,218, 62 ,-255, 255);
 
-   if (rightv > 10 ){
-     analogWrite(rightMotorEnable, rightv);
-     digitalWrite(rightMotorPin1, HIGH);
-     digitalWrite(rightMotorPin2, LOW);
-   }
-   else if (rightv < -10 ){
-     analogWrite(rightMotorEnable, -rightv);
-     digitalWrite(rightMotorPin1, LOW);
-     digitalWrite(rightMotorPin2, HIGH);
-   }
-   else
-   {
-     analogWrite(rightMotorEnable, 0);
-     digitalWrite(leftMotorPin1, LOW);
-     digitalWrite(leftMotorPin2, LOW);
-   }
-//resetting values
+   //int nx = map(x, 62 , 218 ,-255, 255);
+   
+   leftv = constrain(y + x, -255, 255);
+   rightv = constrain(y - x, -255, 255);
+
+
+  if (leftv > 10) { 
+    analogWrite(leftMotorEnable, leftv);
+    digitalWrite(leftMotorPin1, HIGH);
+    digitalWrite(leftMotorPin2, LOW);
+  } 
+  else if (leftv < -10) { 
+    analogWrite(leftMotorEnable, -leftv);
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+  } 
+  else { 
+    analogWrite(leftMotorEnable, 0);
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, LOW);
+  }
+  if (rightv > 10) { 
+    analogWrite(rightMotorEnable, rightv);
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);
+  } 
+  else if (rightv < -10) { 
+    analogWrite(rightMotorEnable, -rightv);
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);
+  } 
+  else { 
+    analogWrite(rightMotorEnable, 0);
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, LOW);
+  }
   leftv=0;
   rightv=0;
   inputX= "";
   inputY= "";
-  delay(50);
+  readingX=true;
+  delay(10);
 }
 
